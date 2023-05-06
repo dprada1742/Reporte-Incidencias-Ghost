d92cf7a0d363a5a6c50ce05de343ac46ecc48c16
Daniela-Prada <d.prada1742@uniandes.edu.co>
2023-05-05 21:41:23 -0500

    Add: Creacion Inicial de la objectPage class

---
+++ Kraken/features/web/support/LoginPage.js
@@ -0,0 +1,26 @@
+class LoginPage {
+    constructor(driver) {
+      this.driver = driver;
+    }
+  
+    async visit(baseUrl) {
+      await this.driver.url(`${baseUrl}ghost/#/signin`);
+    }
+  
+    async fillEmail(value) {
+      const emailField = await this.driver.$('#ember8');
+      await emailField.setValue(value);
+    }
+  
+    async fillPassword(value) {
+      const passwordField = await this.driver.$('#ember10');
+      await passwordField.setValue(value);
+    }
+  
+    async submit() {
+      const submitButton = await this.driver.$('#ember12');
+      await submitButton.click();
+    }
+  }
+  
+  module.exports = LoginPage;
