8503953129875a896dfb868848361ba979c3be9f
daniela <d.prada1742@uniandes.edu.co>
2023-05-05 19:41:16 -0500

    Add: Creacion archivos editar_tag test

---
+++ CypressE2E/cypress/e2e/Tests/editar_tag.cy.js
@@ -0,0 +1,10 @@
+import LoginPage from "../pages/LoginPage";
+import NewTagPage from "../pages/NewTagPage";
+import TagsPage from "../pages/TagsPage";
+import Sidebar from "../pages/Sidebar";
+
+import { faker } from "@faker-js/faker";
+
+describe("Editar tag", () => {
+
+});
