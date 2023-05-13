5c2233375ffa2dc78579bb50e16e019cb94c08e4
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2023-05-12 19:00:04 -0500

    updated 3.41.1 routes

--- CypressE2E/cypress/e2e/Tests 3.4.1/crear_post.cy.js
+++ CypressE2E/cypress/e2e/Tests 3.4.1/crear_post.cy.js
@@ -3,6 +3,8 @@ import PostPage from "../pages 3.4.1/PostPage";
 import NewPostPage from "../pages 3.4.1/NewPostPage";
 import Sidebar from "../pages 3.4.1/Sidebar";
 
+let hasScreenshotBeenTaken = false;
+
 describe("Crear Post", () => {
   beforeEach(() => {
     cy.fixture("loginData").then((data) => {
@@ -11,6 +13,12 @@ describe("Crear Post", () => {
       LoginPage.visit(baseUrl);
       LoginPage.fillEmail(email);
       LoginPage.fillPassword(password);
+
+      if (!hasScreenshotBeenTaken) {
+        cy.screenshot("crear_post_login");
+        hasScreenshotBeenTaken = true;
+      }
+
       LoginPage.submit();
     });
   });
@@ -21,9 +29,12 @@ describe("Crear Post", () => {
 
       // Given: Voy a la seccion de Post
       PostPage.visit(baseUrl);
+      cy.wait(1000);
+      cy.screenshot("sc1_01_visit_post_page")
 
       // When: Oprimo el boton New Post
       PostPage.createNewPost();
+      cy.screenshot("sc1_02_new_post")
 
       // When: Lleno todos los campos del formulario de new post y oprimo el boton publish
       NewPostPage.fillTitle("La Mejores ciudades para visitar en Colombia");
@@ -31,12 +42,15 @@ describe("Crear Post", () => {
                               "Etiam vel eros congue, porta lectus non, lobortis tellus. " + 
                               "Mauris at dui eget ante volutpat elementum at sit amet nunc. "+
                               "Praesent bibendum bibendum diam, consectetur tincidunt metus condimentum eu.");
+      
+      cy.screenshot("sc1_03_fill_post");
 
-      NewPostPage.publish()
-      NewPostPage.comfirmPublish()
+      NewPostPage.publish();
+      NewPostPage.comfirmPublish();
 
       // Then: Recibo Notificacion que el post fue publicado
-      NewPostPage.assertMessage('Published')
+      cy.screenshot("sc1_04_post_created")
+      NewPostPage.assertMessage('Published');
     });
   });  
 
@@ -46,9 +60,12 @@ describe("Crear Post", () => {
 
       // Given: Voy a la seccion de Post
       PostPage.visit(baseUrl);
+      cy.wait(1000);
+      cy.screenshot("sc2_01_visit_post_page")
 
       // When: Oprimo el boton New Post
       PostPage.createNewPost();
+      cy.screenshot("sc2_02_new_post")
 
       // When: Lleno todos los campos del formulario de new post y oprimo el boton publish
       NewPostPage.fillTitle(" ");
@@ -57,10 +74,13 @@ describe("Crear Post", () => {
                               "Mauris at dui eget ante volutpat elementum at sit amet nunc. "+
                               "Praesent bibendum bibendum diam, consectetur tincidunt metus condimentum eu.");
 
+      cy.screenshot("sc2_03_fill_post");
+
       NewPostPage.publish()
       NewPostPage.comfirmPublish()
 
       // Then: Recibo Notificacion que el post fue publicado
+      cy.screenshot("sc2_04_post_created")
       NewPostPage.assertMessage('Published')
     });
   });
@@ -71,18 +91,24 @@ describe("Crear Post", () => {
 
       // Given: Voy a la seccion de Post
       PostPage.visit(baseUrl);
+      cy.wait(1000);
+      cy.screenshot("sc3_01_visit_post_page")
 
       // When: Oprimo el boton New Post
       PostPage.createNewPost();
+      cy.screenshot("sc3_02_new_post")
 
       // When: Lleno todos los campos del formulario de new post y oprimo el boton publish
       NewPostPage.fillTitle("La Mejores ciudades para visitar en Colombia");
       NewPostPage.fillContent(" ");
 
+      cy.screenshot("sc3_03_fill_post");
+
       NewPostPage.publish()
       NewPostPage.comfirmPublish()
 
       // Then: Recibo Notificacion que el post fue publicado
+      cy.screenshot("sc3_04_post_created")
       NewPostPage.assertMessage('Published')
     });
   });
@@ -93,20 +119,26 @@ describe("Crear Post", () => {
 
       // Given: Voy a la seccion de Post
       PostPage.visit(baseUrl);
+      cy.wait(1000);
+      cy.screenshot("sc4_01_visit_post_page")
 
       // When: Oprimo el boton New Post
       PostPage.createNewPost();
+      cy.screenshot("sc4_02_new_post")
 
       // When: Lleno todos los campos del formulario de new post y oprimo el boton publish
       NewPostPage.fillTitle("La Mejores ciudades para visitar en Colombia");
       NewPostPage.fillContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. " + 
                               "Etiam vel eros congue, porta lectus non, lobortis tellus. ")
+      
+      cy.screenshot("sc4_03_fill_post");
 
       NewPostPage.publish()
       NewPostPage.schedule()
       NewPostPage.comfirmPublish()
 
       // Then: Recibo Notificacion que el post fue publicado
+      cy.screenshot("sc4_04_post_scheduled")
       NewPostPage.assertMessage('Scheduled')
       
     });
